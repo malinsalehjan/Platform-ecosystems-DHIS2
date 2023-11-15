f01e706ee7ae6cc4f784039c3ba5b531cfc29a6a
Malin <malinsalehjan@gmail.com>
2023-11-15 22:14:15 +0100

    Fixing merge conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import classes from './App.module.css';
 import InventoryPage from './pages/InventoryPage/InventoryPage';
-<<<<<<< HEAD
 import TrainingPage from './pages/TrainingPage/TrainingPage';
-=======
 import HistoryPage from './pages/HistoryPage/HistoryPage';
->>>>>>> f02ae22 (Feat/history log (#7))
 import RefillPage from './pages/RefillPage/RefillPage';
 import { DHIS2Provider } from './contexts/DHIS2Context';
 import { DHIS2HistoryProvider } from './contexts/DHIS2HistoryContext';
@@ -15,12 +12,7 @@ import { daysUntilDelivery } from './utility/dateUtility';
 import { IconCalendar16 } from '@dhis2/ui';
 
 const App = () => {
-<<<<<<< HEAD
   const [activePage, setActivePage] = useState('Inventory'); 
-=======
-  const [activePage, setActivePage] = useState('Inventory');
-  console.log(activePage);
->>>>>>> f02ae22 (Feat/history log (#7))
 
   function activePageHandler(page) {
     setActivePage(page);
@@ -44,17 +36,12 @@ const App = () => {
             <span>Days until next delivery: {daysUntilDelivery()}</span>
           </div>
           <DHIS2Provider>
-<<<<<<< HEAD
-            {activePage === 'Inventory' && {activePage === 'Inventory' && <InventoryPage />}
-            {activePage === 'TrainingPage' && <TrainingPage />}}
-            {activePage === 'Refill' && <RefillPage />}
-=======
             <DHIS2HistoryProvider>
               {activePage === 'Inventory' && <InventoryPage />}
               {activePage === 'Refill' && <RefillPage />}
               {activePage === 'History' && <HistoryPage />}
+              {activePage === 'TrainingPage' && <TrainingPage />}
             </DHIS2HistoryProvider>
->>>>>>> f02ae22 (Feat/history log (#7))
           </DHIS2Provider>
         </div>
       </AlertProvider>
